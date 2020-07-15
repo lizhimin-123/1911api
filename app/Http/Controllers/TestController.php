bd88ba90b3d1638247f846c02c73e142978cbd9f
lizhimin <2763855398@qq.com>
2020-07-15 06:56:41 +0000

    curl

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //
+    public function Test()
+    {
+    	$appid = "wx70b42c1d1a4ee5b4";
+    	$appsecret = "5c52d652cce1d40cd1a438374def34bf";
+    	$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
+    	$data = file_get_contents($url);
+    	echo $data;
+    }
+
+    public  function getWxToken()
+    {
+
+    	$appid = "wx70b42c1d1a4ee5b4";
+    	$appsecret = "5c52d652cce1d40cd1a438374def34bf";
+    	$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
+    	// 创建一个新cURL资源
+		$ch = curl_init();
+
+		// 设置URL和相应的选项
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//返回结果通过变量接收
+		// 抓取URL并把它传递给浏览器
+		$response = curl_exec($ch);
+
+		// 关闭cURL资源，并且释放系统资源
+		curl_close($ch);
+		echo $response;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::any('/test',"TestController@Test"); 
+Route::any('/getWxToken',"TestController@getWxToken"); 
